c03d6258910bcfb9d2ae9c2beb26e936522e4d88
dhanach <dhanachintrsuk@gmail.com>
2020-10-04 11:55:07 +0700

    update develop

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,5 @@
 <template>
 <div class="hello">
-    // pointless comments
-    // add comment to develop branch after update feature
-    // add comment to develop branch
-    // add comment to test-feature-branch-2
     <h1>{{msg}} </h1>
 </div>
 </template>
